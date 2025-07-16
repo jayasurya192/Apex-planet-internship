26f522d7fa1a2af5ac191aebbb38c10295d70418
jayasurya192 <djsurya2014@gmail.com>
2025-07-16 18:15:31 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,4 @@
+// script.js
+function showFact() {
+  alert("🤖 Fun Fact: In 'Robo', Rajinikanth played both the scientist and the robot, showcasing groundbreaking dual performances in Indian cinema!");
+}
